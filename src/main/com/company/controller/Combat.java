f823885698ff728325ceb1182dab2f139b1ce5bc
Adam <adamwroblewski.1993@gmail.com>
2020-03-11 22:25:17 +0100

    Create Combat class.
    > Simulate fight between two gladiators.
    > Stop simulation after first death.
    > Display info about result and winner new stats.

--- src/main/com/company/controller/Combat.java
+++ src/main/com/company/controller/Combat.java
@@ -1,5 +1,43 @@
 package com.company.controller;
 
+import com.company.model.gladiators.Gladiator;
+import com.company.view.MessageDisplayer;
+
 public class Combat {
 
+    private Gladiator firstGladiator;
+    private Gladiator secondGladiator;
+    private Gladiator winner = null;
+    private Gladiator loser = null;
+
+    private boolean isFightEnd = false;
+    private MessageDisplayer messageDisplayer = new MessageDisplayer();
+
+    public Combat(Gladiator firstGladiator, Gladiator secondGladiator) {
+        this.firstGladiator = firstGladiator;
+        this.secondGladiator = secondGladiator;
+    }
+
+    public void simulateCombat() {
+
+        while (!isFightEnd) {
+            simulateTrun(firstGladiator, secondGladiator);
+            if (isFightEnd) break;
+
+            simulateTrun(secondGladiator, firstGladiator);
+        }
+        assert winner != null;
+        winner.advanceLvl();
+        messageDisplayer.displayMessageAfterWinningCombat(winner, loser);
+    }
+
+    private void simulateTrun(Gladiator attacker, Gladiator defender) {
+        attacker.attack(defender);
+        messageDisplayer.displayCombatMessage(attacker);
+        if (defender.getHP() < 0) {
+            winner = attacker;
+            loser = defender;
+            isFightEnd = true;
+        }
+    }
 }
