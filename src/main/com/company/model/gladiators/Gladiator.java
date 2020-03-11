a01db1bc7e82f89183b7669d252d0fea0faec1ad
Adam <adamwroblewski.1993@gmail.com>
2020-03-11 19:45:55 +0100

    Hit or miss mechanic, decrease enemy gladiator hp after successful attack.

--- src/main/com/company/model/gladiators/Gladiator.java
+++ src/main/com/company/model/gladiators/Gladiator.java
@@ -9,7 +9,7 @@ public abstract class Gladiator {
     private float baseHP;
     private float baseSP;
     private float baseDex;
-    private int lvl;
+    private int lvl = 1;
     private String name;
 
     public Gladiator() {
@@ -23,35 +23,36 @@ public abstract class Gladiator {
     }
 
     private int generateStatistic() {
-        int lowerLimit = 1;
         int upperLimit = 100;
-        return random.nextInt(100) + 1;
+        return random.nextInt(upperLimit) + 1; // + 1 to generate random number in range 1-100
     }
 
-    public void attack() {
+    public void attack(Gladiator enemy) {
+        float unclampedChance = this.getDEX() - enemy.getDEX();
+        float chance = Math.max(10, Math.min(unclampedChance, 100));
         float lowerAttackRange = 0.1F;
         float upperAttackRange = 0.5F;
-        float dmg = (float) (lowerAttackRange + Math.random() * (upperAttackRange - lowerAttackRange));
+        float dmg = (float) (lowerAttackRange + Math.random() * (upperAttackRange - lowerAttackRange)) * this.getSP();
+
+        if (random.nextFloat() * 100 < chance) {
+            enemy.decreaseHP(dmg);
+        }
     }
 
-    private int randomizeStatistic(int lowerRange, int upperRange) {
-        return random.nextInt(upperRange - lowerRange) + lowerRange;
+    private void decreaseHP(float dmg) {
+        this.baseHP -= dmg / lvl;
     }
 
     public float getHP() {
-        return baseHP;
+        return baseHP * lvl;
     }
 
     public float getSP() {
-        return baseSP;
+        return baseSP * lvl;
     }
 
     public float getDEX() {
-        return baseDex;
-    }
-
-    public int getLvl() {
-        return lvl;
+        return baseDex * lvl;
     }
 
     protected void advanceLvl() {
