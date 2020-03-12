041da518d9586ec7ad4d5793074271fbcb4f0971
Adam <adamwroblewski.1993@gmail.com>
2020-03-12 22:30:09 +0100

    Working tournament schedule.

--- src/main/com/company/model/tournamentTree/TournamentSchedule.java
+++ src/main/com/company/model/tournamentTree/TournamentSchedule.java
@@ -6,10 +6,9 @@ import com.company.model.gladiators.Swordsman;
 
 public class TournamentSchedule {
     private Combat[] tournamentSchedule;
-    private static int numberOfFights;
 
     public TournamentSchedule(int numberOfRounds) {
-        numberOfFights = (int) Math.pow(2, numberOfRounds) - 1;
+        int numberOfFights = (int) Math.pow(2, numberOfRounds) - 1;
         tournamentSchedule = new Combat[numberOfFights];
         for (int i = 0; i < tournamentSchedule.length; i++) {
             if (i < tournamentSchedule.length / 2)
@@ -22,32 +21,16 @@ public class TournamentSchedule {
 
     public Gladiator startTournament() {
 
-        while (numberOfFights > 0) {
-            numberOfFights /= 2;
+        for (int i = tournamentSchedule.length - 1; i >= 0; i--) {
+            if (i == 0)
+                return new Combat(tournamentSchedule[1].simulateCombat(),
+                        tournamentSchedule[2].simulateCombat()).simulateCombat();
 
-            for (int i = numberOfFights; i > numberOfFights / 2; i--) {
-//                if (i == 0) {
-//                    tournamentSchedule[0] = new Combat(
-//                            tournamentSchedule[1].simulateCombat(),
-//                            tournamentSchedule[2].simulateCombat()
-//                    );
-//                    return tournamentSchedule[0].simulateCombat();
-//                }
-                if (i == 1) {
-                    System.out.println(i);
-                    tournamentSchedule[i - 1] = new Combat(
-                            tournamentSchedule[i].simulateCombat(),
-                            tournamentSchedule[i + 1].simulateCombat()
-                    );
-                } else {
-                    tournamentSchedule[i] = new Combat(
-                            tournamentSchedule[i + 1].simulateCombat(),
-                            tournamentSchedule[i + 2].simulateCombat()
-                    );
-                }
+            if (tournamentSchedule[i] == null) {
+                tournamentSchedule[i] = new Combat(tournamentSchedule[i * 2 + 1].simulateCombat(),
+                        tournamentSchedule[i * 2 + 2].simulateCombat());
             }
         }
-
         return null;
     }
 }
