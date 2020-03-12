f6a76a2b1580609657e19012bbcd74cffde0821b
Adam <adamwroblewski.1993@gmail.com>
2020-03-12 11:02:40 +0100

    Simple simulation of two gladiators combat.

--- src/main/com/company/Main.java
+++ src/main/com/company/Main.java
@@ -1,20 +1,17 @@
 package com.company;
 
-import com.company.model.gladiators.Assassin;
+import com.company.controller.Combat;
+import com.company.model.gladiators.Brutal;
 import com.company.model.gladiators.Gladiator;
-
-import java.util.Random;
+import com.company.model.gladiators.Swordsman;
 
 public class Main {
 
+
     public static void main(String[] args) {
-        Gladiator swordsman = new Assassin();
-        System.out.println("hp: " + swordsman.getHP());
-        System.out.println("sp: " + swordsman.getSP());
-        System.out.println("dex: " + swordsman.getDEX());
-        for (int i = 0; i < 10; i++) {
-            Random random = new Random();
-            System.out.println(0.1 + Math.random() * (0.5 - 0.1));
-        }
+        Gladiator swordsman = new Swordsman();
+        Gladiator brutal = new Brutal();
+        Combat combat = new Combat(swordsman, brutal);
+        combat.simulateCombat();
     }
 }
